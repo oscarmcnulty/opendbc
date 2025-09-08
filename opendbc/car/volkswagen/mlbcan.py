c6cd137a3930b95ef3bfaf4f5b75a5c214c06157
Oscar McNulty <2888339+oscarmcnulty@users.noreply.github.com>
2025-09-08 00:56:33 -0700

    VW MLB: Add ACC_01 signal and checksum

--- opendbc/car/volkswagen/mlbcan.py
+++ opendbc/car/volkswagen/mlbcan.py
@@ -83,6 +83,7 @@ def create_acc_hud_control(packer, bus, acc_hud_status, set_speed, lead_distance
 
 def volkswagen_mlb_checksum(address: int, sig, d: bytearray) -> int:
   xor_starting_value = {
+    0x109: 0x08, # ACC_01
     0x30C: 0x0F, # ACC_02
     0x324: 0x27, # ACC_04
     0x10D: 0x0C, # ACC_05
